21afaf2883654348a96d17da24431c8e9d9be281
haspimenta <hgalsilva@gmail.com>
2020-05-01 20:54:42 +0100

    bucketsort
    codigo bucket sort, inicio do codigo em algoritmo.

---
+++ src/BucketSort.kt
@@ -0,0 +1,43 @@
+import java.util.*
+import kotlin.collections.ArrayList
+
+
+class BucketSort {
+    object BucketSort {
+        fun bucketSort(input: IntArray): IntArray {
+            val bucketsort = divBcs(input)
+            val buckets: Array<MutableList<*>> = arrayOfNulls(bucketsort[1])
+            for (i in 0 until bucketsort[1]) {
+                buckets[i] = ArrayList<Any?>()
+            }
+            for (i in input) {
+                buckets[divBcs(i, bucketsort)].add(i)
+            } //java.lang.IndexOutOfBoundsException : Invalid array index: 80 -- a cada valor verificado
+            for (bucket in buckets) {
+                Collections.sort(bucket)
+            }
+            var posicao = 0
+            for (n in buckets.indices) {
+                for (v in buckets[n]) {
+                    input[posicao++] = v as Int
+                }
+            }
+            return input
+        }
+
+        private fun divBcs(input: IntArray): IntArray {
+            var m = input[0]
+            for (i in 1 until input.size) {
+                if (m < input[i]) {
+                    m = input[i]
+                }
+            }
+            return intArrayOf(m, Math.sqrt(input.size.toDouble()).toInt())
+        }
+
+        private fun divBcs(i: Int, bucketsort: IntArray): Int {
+            return (i.toDouble() / bucketsort[0] * (bucketsort[1] - 1)).toInt()
+        }
+    }
+
+}
---
+++ src/Insertion.kt
@@ -0,0 +1,8 @@
+import BucketSort.BucketSort
+import java.util.*
+import kotlin.Array
+
+
+class Insertion {
+    
+}
