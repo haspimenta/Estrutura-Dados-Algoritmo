08bb4a8f14d910154a60da64d807e659b0b7b6a7
haspimenta <hgalsilva@gmail.com>
2020-05-02 02:00:40 +0100

    insertion
    adicionando a função insertion no codigo do bucketsort

--- src/BucketSort.kt
+++ src/BucketSort.kt
@@ -1,24 +1,24 @@
-import java.util.*
 import kotlin.collections.ArrayList
-
+import kotlin.collections.*
+import kotlin.intArrayOf as kotlinIntArrayOf
 
 class BucketSort {
     object BucketSort {
         fun bucketSort(input: IntArray): IntArray {
             val bucketsort = divBcs(input)
-            val buckets: Array<MutableList<*>> = arrayOfNulls(bucketsort[1])
+            val buckets: Array<MutableList<*>?> = arrayOfNulls(bucketsort[1])
             for (i in 0 until bucketsort[1]) {
                 buckets[i] = ArrayList<Any?>()
             }
             for (i in input) {
-                buckets[divBcs(i, bucketsort)].add(i)
+                buckets[divBcs(i, bucketsort)]!!
             } //java.lang.IndexOutOfBoundsException : Invalid array index: 80 -- a cada valor verificado
             for (bucket in buckets) {
-                Collections.sort(bucket)
+                kotlinIntArrayOf().sort(bucket)
             }
             var posicao = 0
             for (n in buckets.indices) {
-                for (v in buckets[n]) {
+                for (v in buckets[n]!!) {
                     input[posicao++] = v as Int
                 }
             }
@@ -32,12 +32,16 @@ class BucketSort {
                     m = input[i]
                 }
             }
-            return intArrayOf(m, Math.sqrt(input.size.toDouble()).toInt())
+            return kotlinIntArrayOf(m, Math.sqrt(input.size.toDouble()).toInt())
         }
 
         private fun divBcs(i: Int, bucketsort: IntArray): Int {
             return (i.toDouble() / bucketsort[0] * (bucketsort[1] - 1)).toInt()
         }
     }
+
+}
+
+private fun IntArray.sort(fromIndex: MutableList<*>?) {
 
 }
--- src/Insertion.kt
+++ src/Insertion.kt
@@ -2,7 +2,31 @@ import BucketSort.BucketSort
 import java.util.*
 import kotlin.Array
 
-
 class Insertion {
-    
+    var bucketSort = BucketSort()
+
+    companion object {
+        //private val input: IntArray = TODO()
+
+        @JvmStatic
+        fun main(args: Array<String>) {
+            println("EDA - Bucket-Sort")
+            val input = intArrayOf(80, 50, 30, 10, 90, 60, 0, 70, 40, 20, 50)
+            println("Entrada de dados desordenados")
+            println(Arrays.toString(input))
+
+            //insertionSort(input)
+
+            println("Saida dos dados ordenado")
+            println(Arrays.toString(BucketSort.bucketSort(input)))
+        }
+
+        //verifica o tamanho da entrada
+        private fun insertionSort(input: IntArray) {
+            for (i in 1 until input.size) {
+                val aux = input[i]
+                BucketSort.bucketSort(input)
+            }
+        }
+    }
 }
