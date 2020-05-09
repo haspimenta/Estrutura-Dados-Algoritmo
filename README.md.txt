b3d9e54976f200999cc80cb20be6429669cfbc84
haspimenta <hgalsilva@gmail.com>
2020-05-09 10:10:14 +0100

    Criando arquivos md
    Criando arquivos markdown e reorganizando arquivos nas devidas pastas.

---
+++ README.md.txt
@@ -0,0 +1,22 @@
+EDA - Algoritmos e Estruturas de Dados
+IPBEJA - ESTIG
+
+Professor: Jasnau
+
+Aluno: Hugo Alexandre Silva
+N: 18544
+E-mail: 18544@stu.ipbeja.pt
+
+Este repositório disponibiliza alguns algoritmos e estruturas de dados conforme disponibilizados na cadeira.
+Além disso, disponibiliza também graficos feito em Gnuplot de desenvolvimento e crescimento das taxas de execução.
+
+Tem como objetivo desenvolver os problemas elucidados para o projeto da melhor forma possível, alcançando as melhores
+performances dos algoritmos que são disponibilizados no decorrer das aulas e na linguagem: Kotlin.
+
+Estruturas de Dados			
+Árvore de pesquisa binária		Kotlin
+Classes red-black			Kotlin
+
+Algoritmos de Ordenação	
+Bucket Sort				Kotlin
+Insertion Sort				Kotlin	
--- src/BucketMain.kt
+++ src/BucketMain.kt
@@ -8,9 +8,11 @@ import org.opencv.imgcodecs.Imgcodecs
 import java.util.*
 import kotlin.collections.ArrayList
 
-object BucketMain {
+object BucketMain
+{
     @JvmStatic
-    fun main(args: Array<String>) {
+    fun main(args: Array<String>)
+    {
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME)
         println("EDA - Bucket-Sort")
         val N_BUCKETS = 100 //numero de buckets
@@ -20,64 +22,72 @@ object BucketMain {
         PrintBuckets(buckets) //imprimir resultado (test)
     }
 
-    private fun GetPixelValues(path: String): ArrayList<Int> {
+    //criando a array de pixels da imagem inserida, através de linhas e colunas
+    private fun GetPixelValues(path: String): ArrayList<Int>
+    {
         val img = Imgcodecs.imread(path, Imgcodecs.IMREAD_GRAYSCALE) //FULL HD 1920*1080
         val values = ArrayList<Int>()
-        for (r in 0 until img.rows() - 1) {
-            for (c in 0 until img.cols() - 1) {
+        for (r in 0 until img.rows() - 1)
+        {
+            for (c in 0 until img.cols() - 1)
+            {
                 val temp = img[r, c]
                 values.add(temp[0].toInt())
             }
         }
         return values
     }
-
-    private fun BucketSort(toSort: ArrayList<Int>, nBuckets: Int): ArrayList<ArrayList<Int>> {
-
+    //iniciando o algoritmo bucketsort com os devidos passos
+    private fun BucketSort(toSort: ArrayList<Int>, nBuckets: Int): ArrayList<ArrayList<Int>>
+    {
         //iniciar buckets
         val buckets = ArrayList<ArrayList<Int>>()
-
         for (i in 0 until nBuckets)
         {
             buckets.add(ArrayList<Int>())
         }
-
         //encontrar o valor máximo do array para ordenar
         var max = 0
-        for (i in toSort.indices) {
-            if (toSort[i] > max) {
+        for (i in toSort.indices)
+        {
+            if (toSort[i] > max)
+            {
                 max = toSort[i]
             }
         }
-
         //distribuir itens pelos buckets
-        for (i in toSort.indices) {
+        for (i in toSort.indices)
+        {
             val nBucketDest = Math.floor(toSort[i] / (max + 1.0) * nBuckets).toInt()
             buckets.get(nBucketDest).add(toSort[i])
         }
-
         //aplicar algoritmo insertion sort a cada bucket
-        for (i in buckets.indices) {
+        for (i in buckets.indices)
+        {
             InsertionSort(buckets[i]) //as ArrayList<Int>)
         }
-
         return buckets
     }
-
-    private fun InsertionSort(bucket: ArrayList<Int>) {
-        for (n in bucket.indices) {
+    //aplicando o insertion a cada bucket com a sua devida ordenação
+    private fun InsertionSort(bucket: ArrayList<Int>)
+    {
+        for (n in bucket.indices)
+        {
             var j = n
             val temp = bucket[n]
-            while (j > 0 && bucket[j - 1] > temp) {
+            while (j > 0 && bucket[j - 1] > temp)
+            {
                 bucket[j] = bucket[j - 1]
                 j--
             }
             bucket[j] = temp
         }
     }
-
-    private fun PrintBuckets(buckets: ArrayList<ArrayList<Int>>) {
-        for (i in buckets.indices) {
+    //imprimindo os resultados obtidos
+    private fun PrintBuckets(buckets: ArrayList<ArrayList<Int>>)
+    {
+        for (i in buckets.indices)
+        {
             println("bucket " + i + ": " + Arrays.toString(buckets[i].toArray())); //imprive valores dentro dos buckets
             println("bucket " + i + ": " + buckets[i].size) //imprime tamanho dos buckets
         }
